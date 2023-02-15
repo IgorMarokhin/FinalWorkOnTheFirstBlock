09afae4d465e15e98f8938d3a129ca370c5daebe
IgorMarokhin <lawyer.mii222@gmail.com>
2023-02-15 19:25:32 +0300

    В файл FinalTask добавлено решение задачи

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина
+// которых меньше или равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо
+// задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+//
+// Примеры:
+//
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+//
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// 
+// ["Russia", "Denmark", "Kazan"] -> []
+
+string[] array = { "hello", "1632", "13", "my", "5101", "18", "dear", "4151", "41", "friend" };
+Console.Write($"[{String.Join(", ", array)}]");
+
+string[] arrayThreeChar = FindThreeCharElement(array);
+
+Console.WriteLine($" -> [{String.Join(", ", arrayThreeChar)}]");
+
+/////////////////////////////////////////////
+
+string[] FindThreeCharElement(string[] array)
+{
+    string[] result = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[i] = array[i];
+        }
+    }
+    result = result.Where(x => x != null).ToArray();
+    return result;
+}
